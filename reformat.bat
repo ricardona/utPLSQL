c9f8e604df1c77500325febc4d612efe90b465da
David Pyke <shoelace_822695@hotmail.com>
2016-07-22 10:08:35 +0100

    improve reformat.bat to use env variable

--- reformat.bat
+++ reformat.bat
@@ -1,1 +1,18 @@
-for /R source %%i in (*.pks *.pkb *.tps *.tpb) do C:\Apps\sqldeveloper\sqldeveloper\bin\sdcli format input=%%i output=%%i
+@echo off
+VERIFY OTHER 2>NUL
+SETLOCAL ENABLEEXTENSIONS
+IF ERRORLEVEL 1 ECHO Unable to enable extensions
+SET me=%~n0
+SET parent=%~dp0
+
+IF NOT DEFINED SQLDEVELOPER_HOME (ECHO SQLDEVELOPER_HOME is NOT defined. please set it such that %%SQLDEVELOPER_HOME%%\sqldeveloper\bin\sdcli is valid )
+IF NOT DEFINED SQLDEVELOPER_HOME (exit /B 1 )
+
+set SDCLI=%SQLDEVELOPER_HOME%\sqldeveloper\bin\sdcli
+IF NOT EXIST %SDCLI% (echo executable not found:%SDCLI% && exit /B 2 )
+
+
+for /R source %%i in (*.pks *.pkb *.tps *.tpb) do (echo %%i && %SDCLI% format input=%%i output=%%i )
+
+
+endlocal
